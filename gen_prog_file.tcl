431fe9827a18e229548c5e22dbb33944f5f49e11
Tomas Šervenikas <t.servenikas@limemicro.com>
2023-02-17 10:52:33 +0200

    Added DAC control functionality, fixed microblaze linker issues

--- gen_prog_file.tcl
+++ gen_prog_file.tcl
@@ -9,7 +9,7 @@ puts $bit_path
 set bit_string "up 0x00000000 $bit_path "
 puts $bit_string
 
-write_cfgmem  -format bin -force -size 16 -interface SPIx4 -loadbit $bit_string -file "[file normalize "$script_path/bitstream/flash_programming_file.bin"]"
+write_cfgmem  -format bin -force -size 32 -interface SPIx4 -loadbit $bit_string -file "[file normalize "$script_path/bitstream/flash_programming_file.bin"]"
 file copy -force $bit_path $script_path/bitstream/ram_programming_file.bit
 
 source $script_path/update_readme.tcl
