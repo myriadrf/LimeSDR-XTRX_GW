b543efa01611238302ea749c252963b0dc07f5ac
vbuitvydas <v.buitvydas@limemicro.com>
2023-08-31 11:21:38 +0300

    Updated programming files

---
+++ gen_prog_file_debug.tcl
@@ -0,0 +1,16 @@
+# Set the reference directory to where the script is
+set script_path [file dirname [file normalize [info script]]]
+puts $script_path
+
+# Set output file path and name
+set bit_path "[file normalize "$script_path/projects/LimeSDR-XTRX-Aurora/LimeSDR-XTRX-Aurora.runs/impl_1/LimeSDR_XTRX_top.bit"]"
+puts $bit_path
+
+set bit_string "up 0x00000000 $bit_path "
+puts $bit_string
+
+write_cfgmem  -format bin -force -size 32 -interface SPIx4 -loadbit $bit_string -file "[file normalize "$script_path/bitstream/flash_programming_file.bin"]"
+file copy -force $bit_path $script_path/bitstream/ram_programming_file.bit
+# removed this in order not to increase revision while debuging 
+#source $script_path/update_readme.tcl
+#source $script_path/update_rev.tcl
