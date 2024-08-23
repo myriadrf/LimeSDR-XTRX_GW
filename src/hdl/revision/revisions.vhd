13b58dc0357e5e98d8760b44a86d3e25ad168a59
vbuitvydas <v.buitvydas@limemicro.com>
2024-08-23 11:41:49 +0300

    Updated programming files

--- src/hdl/revision/revisions.vhd
+++ src/hdl/revision/revisions.vhd
@@ -14,11 +14,11 @@ package revisions is
 	constant major_rev : integer := 1; 
 	constant minor_rev : integer := 1;
 	constant beta_rev : integer := 1;
-	constant compile_rev : integer := 3;
-	constant compile_year_stamp : integer := 23;
+	constant compile_rev : integer := 4;
+	constant compile_year_stamp : integer := 24;
 	constant compile_month_stamp : integer := 08;
-	constant compile_day_stamp : integer := 24;
-	constant compile_hour_stamp : integer := 13;
+	constant compile_day_stamp : integer := 23;
+	constant compile_hour_stamp : integer := 10;
 	
 	constant magic_num : std_logic_vector(31 downto 0) := x"d8a5f009";
    constant board_id : std_logic_vector(15 downto 0) := 16d"27"; -- 5g radio
